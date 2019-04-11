3179e33f654840205f6015bea71f0b312fad8bef
jahidhasan17 <41954263+jahidhasan17@users.noreply.github.com>
2019-04-12 01:01:27 +0600

    Add files via upload

---
+++ Read_file.java
@@ -0,0 +1,54 @@
+
+package read_file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Read_file 
+{
+    public static void main(String[] args) 
+    {
+        BufferedReader br = null;
+        try 
+        {
+            br = new BufferedReader(new FileReader("/home/jahid/read.html"));
+            String s;
+            String[] words = null;
+            int count = 1;
+            while((s = br.readLine()) != null) 
+            {
+                words = s.split(" ");
+                for(int i=0; i<words.length; i++) 
+                {
+                    if(words[i].equals("")) continue;
+                    for(int j=i+1; j<words.length; j++) 
+                    {
+                        if(words[i].equals(words[j])) 
+                        {
+                            words[j] = "";
+                            count++;
+                        }
+                    }
+                    System.out.println("'" + words[i] + "'" + " is " + count + " times.");
+                    words[i] = "";
+                    count = 1;
+                }
+            }
+        } 
+        catch(IOException e) 
+        {
+            e.printStackTrace();
+        } 
+        finally 
+        {
+            try 
+            {
+                br.close();
+            } catch(IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
